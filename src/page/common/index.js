80664dde104dad8419af5c490e000c59200d7b28
francis <jmgs111@163.com>
2017-08-04 02:14:15 +0800

    Signed-off-by: francis <jmgs111@163.com>

---
+++ src/page/common/index.js
@@ -0,0 +1,9 @@
+/*
+* @Author: francis
+* @Date:   2017-08-02 13:36:48
+* @Last Modified by:   francis
+* @Last Modified time: 2017-08-02 13:38:01
+*/
+'use strict';
+
+console.log('I am global.');
---
+++ src/page/moudle.js
@@ -0,0 +1,10 @@
+/*
+* @Author: francis
+* @Date:   2017-08-02 13:30:26
+* @Last Modified by:   francis
+* @Last Modified time: 2017-08-02 13:31:33
+*/
+
+module.exports = {
+	test: 1234
+}
